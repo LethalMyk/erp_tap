b45308c1c0fa1ece00e2e273196111e190d0cdc2
Mayke <mayke.fleury@gmail.com>
2025-07-09 23:38:34 -0300

    migration table

--- database/migrations/2025_07_10_011711_alterar_enum_forma_pagamento.php
+++ database/migrations/2025_07_10_011711_alterar_enum_forma_pagamento.php
@@ -4,16 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AtualizarEnumFormaPagamento extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
-
-        
+        Schema::table('pagamentos', function (Blueprint $table) {
+            $table->enum('forma', [
+                'PIX',
+                'DEBITO',
+                'DINHEIRO',
+                'CREDITO À VISTA',
+                'CREDITO PARCELADO',
+                'BOLETO',
+                'CHEQUE',
+                'NA ENTREGA',
+                'A PRAZO',
+                'OUTROS'
+            ])->change();
+        });
     }
 
     /**
@@ -21,6 +32,18 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('pagamentos', function (Blueprint $table) {
+            // Reverte para os valores anteriores
+            $table->enum('forma', [
+                'PIX',
+                'DEBITO',
+                'DINHEIRO',
+                'CREDITO À VISTA',
+                'CREDITO PARCELADO',
+                'BOLETO',
+                'CHEQUE',
+                'OUTROS'
+            ])->change();
+        });
     }
-};
+}
